02511672a5ccfeba883785a5f32ce955df84f51f
Anousone Kaseumsouk and Lan Dam <lan@>
2018-06-05 09:56:34 -0700

    Map the input to a new array replace vowels with dash and output as string.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,25 @@ $(document).ready(function(){
   $("#word-puzzle").submit(function(event){
     event.preventDefault();
     var stringInput = $("input#puzzle").val();
-    
+    var inputArray = stringInput.split("");
+
+    var outputArray = inputArray.map(function(letter){
+      if(letter.toUpperCase() === "A"||letter.toUpperCase() === "E"||letter.toUpperCase() === "I"||letter.toUpperCase() === "O"||letter.toUpperCase() === "U"){
+        return "-";
+      } else {
+        return letter;
+      };
+    });
+
+    var outputString = "";
+    for (count = 0; count < outputArray.length; count +=1){
+      outputString = outputString.concat(outputArray[count]);
+      debugger;
+    }
+
+    document.getElementById("word-puzzle").reset();
+
+    $("#output").text(outputString);
 
   });
 });
